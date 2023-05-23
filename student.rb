8e85195b1091a533d454e94f71a1b03514d8e9e0
Samson Tush <samo4421@icloud.com>
2023-05-23 17:50:21 +0300

    Add ruby files

---
+++ student.rb
@@ -0,0 +1,16 @@
+require_relative 'person'
+
+module StudentModule
+  class Student < Person
+    attr_accessor :classroom
+
+    def initialize(id, age, classroom, name = "Unknown", parent_permission = true)
+      super(id, age, name, parent_permission)
+      @classroom = classroom
+    end
+
+    def play_hooky
+      "¯\\(ツ)/¯"
+    end
+  end
+end
